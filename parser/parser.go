e6dc5db9139c5667e9dfc1b0c73edbebb9c23abf
Iryna Shustava <ishustava@gmail.com>
2017-09-05 10:33:11 -0700

    parser.go: ParseVarsStoreFile -> FindCredentials
    FindCredentials is no longer responsible for parsing vars store yml file

--- parser/parser.go
+++ parser/parser.go
@@ -1,27 +1,14 @@
 package parser
 
 import (
-	"io/ioutil"
 	"errors"
 	"gopkg.in/yaml.v2"
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials/values"
 	. "github.com/ishustava/migrator/credentials"
 )
 
-func ParseVarsStoreFile(path string) (*Credentials, error) {
+func FindCredentials(varsStore map[string]interface{}) (*Credentials, error) {
 	creds := new(Credentials)
-
-	varsStoreYaml, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, errors.New("Error occurred when reading vars store file: " + err.Error())
-	}
-
-	varsStore := make(map[string]interface{})
-
-	err = yaml.Unmarshal(varsStoreYaml, &varsStore)
-	if err != nil {
-		return nil, errors.New("Could not unmarshal vars store file: " + err.Error())
-	}
 
 	for name, value := range varsStore {
 		passwordValue, ok := value.(string)
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -10,13 +10,18 @@ import (
 )
 
 var _ = Describe("Parser", func() {
-	Describe("#ParseVarsStoreFile", func() {
+	Describe("#FindCredentials", func() {
 		Context("Passwords", func() {
 			It("finds and returns password credentials", func() {
 				password1 := credentials.NewPassword("path1", "password1")
 				password2 := credentials.NewPassword("path2", "password2")
 
-				creds, err := parser.ParseVarsStoreFile("../test_fixtures/valid_creds.yml")
+				varsStore := map[string]interface{}{
+					"path1": "password1",
+					"path2": "password2",
+				}
+
+				creds, err := parser.FindCredentials(varsStore)
 
 				Expect(err).ToNot(HaveOccurred())
 				Expect(creds.Passwords).To(ConsistOf(password1, password2))
@@ -28,7 +33,19 @@ var _ = Describe("Parser", func() {
 				cert1 := credentials.NewCertificate("path3", values.Certificate{Ca: CA1, Certificate: CERT1, PrivateKey: PRIV1})
 				cert2 := credentials.NewCertificate("path4", values.Certificate{Certificate: CERT2, PrivateKey: PRIV2})
 
-				creds, err := parser.ParseVarsStoreFile("../test_fixtures/valid_creds.yml")
+				varsStore := map[string]interface{}{
+					"path3": map[interface{}]interface{}{
+						"ca": CA1,
+						"certificate": CERT1,
+						"private_key": PRIV1,
+					},
+					"path4": map[interface{}]interface{}{
+						"certificate": CERT2,
+						"private_key": PRIV2,
+					},
+				}
+
+				creds, err := parser.FindCredentials(varsStore)
 
 				Expect(err).ToNot(HaveOccurred())
 				Expect(creds.Certificates).To(ConsistOf(cert1, cert2))
@@ -39,7 +56,15 @@ var _ = Describe("Parser", func() {
 			It("finds and returns ssh credentials", func() {
 				ssh := credentials.NewSsh("path5", values.SSH{PublicKey: SSH_PUB, PrivateKey: SSH_PRIV})
 
-				creds, err := parser.ParseVarsStoreFile("../test_fixtures/valid_creds.yml")
+				varsStore := map[string]interface{}{
+					"path5": map[interface{}]interface{}{
+						"public_key": SSH_PUB,
+						"private_key": SSH_PRIV,
+						"public_key_fingerprint": "fingerprint",
+					},
+				}
+
+				creds, err := parser.FindCredentials(varsStore)
 
 				Expect(err).ToNot(HaveOccurred())
 				Expect(creds.SshKeys).To(ConsistOf(ssh))
@@ -50,7 +75,14 @@ var _ = Describe("Parser", func() {
 			It("finds and returns rsa credentials", func() {
 				rsa := credentials.NewRsa("path6", values.RSA{PublicKey: RSA_PUB, PrivateKey: RSA_PRIV})
 
-				creds, err := parser.ParseVarsStoreFile("../test_fixtures/valid_creds.yml")
+				varsStore := map[string]interface{}{
+					"path6": map[interface{}]interface{}{
+						"public_key": RSA_PUB,
+						"private_key": RSA_PRIV,
+					},
+				}
+
+				creds, err := parser.FindCredentials(varsStore)
 
 				Expect(err).ToNot(HaveOccurred())
 				Expect(creds.RsaKeys).To(ConsistOf(rsa))
