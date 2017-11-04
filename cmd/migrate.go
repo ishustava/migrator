90b31b47c5c697362706719aa7fd376db2d31f0e
Iryna Shustava <ishustava@gmail.com>
2017-11-04 15:11:46 -0700

    Add short flags for director-name and deployment-name

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -16,8 +16,8 @@ type MigrateCommand struct {
 	CredHubURL          string   `short:"u" long:"credhub-url" description:"URL of the CredHub server, e.g. https://example.com:8844" env:"CREDHUB_URL"`
 	CredHubClient       string   `short:"c" long:"credhub-client" description:"UAA client for the CredHub Server" env:"CREDHUB_CLIENT"`
 	CredHubClientSecret string   `short:"s" long:"credhub-client-secret" description:"UAA client secret for the CredHub Server" env:"CREDHUB_CLIENT_SECRET"`
-	DirectorName        string   `long:"director-name" description:"Name of the BOSH director"`
-	DeploymentName      string   `long:"deployment-name" description:"Name of the BOSH deployment with which vars store is used"`
+	DirectorName        string   `short:"e" long:"director-name" description:"Name of the BOSH director"`
+	DeploymentName      string   `short:"d" long:"deployment-name" description:"Name of the BOSH deployment with which vars store is used"`
 	CaCerts             []string `long:"ca-cert" description:"Trusted CA for API and UAA TLS connections. Multiple flags may be provided." env:"CREDHUB_CA_CERT"`
 }
 
