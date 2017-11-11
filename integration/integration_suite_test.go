c6ee282ea60cbc8369466c3dce08ea2569b5324a
Ashwin Venkatesh <ashwin.what@gmail.com>
2017-11-11 17:03:08 -0500

    Delete test helper and move behavior to suite_test.
    Add dummy integration test
    
    Please enter the commit message for your changes. Lines starting

--- integration/integration_suite_test.go
+++ integration/integration_suite_test.go
@@ -4,17 +4,27 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
-	"github.com/ishustava/migrator/test_fixtures"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gexec"
+	"os/exec"
+	"path"
+	"gopkg.in/yaml.v2"
 )
 
 var (
-	homeDir string
-	cfg     test_fixtures.Config
+	homeDir     string
+	TestConfig  Config
+	CommandPath string
 )
+
+type Config struct {
+	ApiUrl       string `yaml:"credhub_api"`
+	CredhubCA    []string `yaml:"credhub_ca_cert"`
+	ClientName   string `yaml:"credhub_client"`
+	ClientSecret string `yaml:"credhub_secret"`
+}
 
 func TestCommands(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -28,13 +38,8 @@ var _ = BeforeEach(func() {
 
 	os.Setenv("HOME", homeDir)
 
-	cfg, err = test_fixtures.LoadConfig()
+	TestConfig, err = LoadConfig()
 	Expect(err).NotTo(HaveOccurred())
-
-	// These happen before each test due to the lack of a BeforeAll
-	// (https://github.com/onsi/ginkgo/issues/70) :(
-	// If the tests are slow, they should be runnable in parallel with the -p option.
-	test_fixtures.TargetAndLogin(cfg)
 })
 
 var _ = AfterEach(func() {
@@ -47,9 +52,45 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 
 	return []byte(path)
 }, func(data []byte) {
-	test_fixtures.CommandPath = string(data)
+	CommandPath = string(data)
 })
 
 var _ = SynchronizedAfterSuite(func() {}, func() {
 	CleanupBuildArtifacts()
 })
+
+func RunCommand(args ...string) *Session {
+	cmd := exec.Command(CommandPath, args...)
+	if os.Getenv("CONFIG") != "" {
+		existing := os.Environ()
+		existing = append(existing, "CREDHUB_URL="+string(TestConfig.ApiUrl))
+		existing = append(existing, "CREDHUB_CLIENT="+string(TestConfig.ClientName))
+		existing = append(existing, "CREDHUB_SECRET="+string(TestConfig.ClientSecret))
+		for _, cert := range TestConfig.CredhubCA {
+			existing = append(existing, "CREDHUB_CA_CERT="+string(cert))
+		}
+		cmd.Env = existing
+	}
+	session, err := Start(cmd, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	<-session.Exited
+
+	return session
+}
+
+func LoadConfig() (Config, error) {
+	configuration := Config{}
+	configFilePath := os.Getenv("CONFIG")
+
+	configurationYaml, err := ioutil.ReadFile(path.Join(os.Getenv("PWD"), configFilePath))
+	if err != nil {
+		return configuration, err
+	}
+
+	err = yaml.Unmarshal(configurationYaml, &configuration)
+	if err != nil {
+		return configuration, err
+	}
+
+	return configuration, nil
+}
